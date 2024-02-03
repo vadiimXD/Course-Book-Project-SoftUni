238f3334919c22432a903c0dafda57a2bcb51388
vadiimXD <vadim123bg@gmail.com>
2024-02-03 15:18:14 +0200

    Created register functionality

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
-//controller woprk with services
+const authService = require("../services/authService")
 
 router.get("/register", (req, res) => {
     res.render("register", { layout: false })
 })
+
+router.post("/register", async (req, res) => {
+    try {
+        await authService.registerUser(req.body.email, req.body.password, req.body)
+        res.redirect("/login")
+    } catch (error) {
+        console.log(error)
+        res.redirect("/404")
+    }
+})
+
 
 module.exports = router
