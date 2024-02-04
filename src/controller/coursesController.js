04ca0cf3eafa1ce3d42989e97c3e3ebc68991d9d
vadiimXD <vadim123bg@gmail.com>
2024-02-04 13:12:04 +0200

    Added static catalog page

--- src/controller/coursesController.js
+++ src/controller/coursesController.js
@@ -1,4 +1,7 @@
 const router = require("express").Router();
 //controller woprk with services
 
+router.get("/courses", (req, res) => {
+    res.render("catalog", { layout: false })
+})
 module.exports = router
