37ee145409c079f95784afee754442569ba4b327
vadiimXD <vadim123bg@gmail.com>
2024-02-08 22:15:40 +0200

    Added error handling to static controller

--- src/controller/staticController.js
+++ src/controller/staticController.js
@@ -2,8 +2,13 @@ const router = require("express").Router();
 const courseService = require("../services/courseService")
 
 router.get("/", async (req, res) => {
-    const lastCourses = await courseService.getLastCourses().lean()
-    res.render("home", { layout: false, lastCourses })
+    try {
+        const lastCourses = await courseService.getLastCourses().lean()
+        res.render("home", { layout: false, lastCourses })
+    } catch (error) {
+        let errorMess = getErrorMessage(error)
+        res.render(`404`, { layout: false, error: errorMess })
+    }
 })
 
 module.exports = router
