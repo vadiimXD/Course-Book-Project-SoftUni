a031d0841bd9183a04f911bbb188baa4f4464f87
vadiimXD <vadim123bg@gmail.com>
2024-02-08 22:09:40 +0200

    Added error handling to course controller

--- src/controller/coursesController.js
+++ src/controller/coursesController.js
@@ -1,10 +1,16 @@
 const router = require("express").Router();
 const { isAuth } = require("../middlewares/authMiddlewares");
-const courseService = require("../services/courseService")
+const courseService = require("../services/courseService");
+const { getErrorMessage } = require("../utils/errorUtils");
 
 router.get("/courses", async (req, res) => {
-    const courses = await courseService.getAllCourses().lean()
-    res.render("catalog", { layout: false, courses })
+    try {
+        const courses = await courseService.getAllCourses().lean()
+        res.render("catalog", { layout: false, courses })
+    } catch (error) {
+        let errorMess = getErrorMessage(error)
+        res.render("404", { layout: false, error: errorMess })
+    }
 })
 
 router.get("/create", isAuth, (req, res) => {
@@ -18,31 +24,44 @@ router.post("/create", isAuth, async (req, res) => {
         await courseService.createCourse(body, req.user.userId)
         res.redirect("/")
     } catch (error) {
-        res.redirect("/404")
+        let errorMess = getErrorMessage(error)
+        res.render("404", { layout: false, error: errorMess })
     }
 })
 
 router.get("/details/:courseId", async (req, res) => {
-    let course = await courseService.getCurrentCourse(req.params.courseId).populate("owner").populate("singUpList").lean()
-    const isOwner = course.owner._id == req.user?.userId
-    let isSigned = courseService.signed(course.singUpList, res.locals.isAuthenticated, req.user.userId);
-    let signed = courseService.isSingUpList(course.singUpList)
+    try {
+        let course = await courseService.getCurrentCourse(req.params.courseId).populate("owner").populate("singUpList").lean()
+        const isOwner = course.owner._id == req.user?.userId
+        let isSigned = courseService.signed(course.singUpList, res.locals.isAuthenticated, req.user.userId);
+        let signed = courseService.isSingUpList(course.singUpList)
 
-    res.render("details", { layout: false, course, isOwner, isSigned, signed })
+        res.render("details", { layout: false, course, isOwner, isSigned, signed })
+    } catch (error) {
+        let errorMess = getErrorMessage(error)
+        const lastCourses = await courseService.getLastCourses().lean()
+        res.render("home", { layout: false, lastCourses, error: errorMess })
+    }
 })
 
 router.get("/sign/:courseId", async (req, res) => {
-    console.log(req.params.courseId)
-    console.log(req.user)
-
-    await courseService.singUp(req.params.courseId, req.user.userId)
-
-    res.redirect(`/details/${req.params.courseId}`)
+    try {
+        await courseService.singUp(req.params.courseId, req.user.userId)
+        res.redirect(`/details/${req.params.courseId}`)
+    } catch (error) {
+        let errorMess = getErrorMessage(error)
+        res.render(`404`, { layout: false, error: errorMess })
+    }
 })
 
 router.get("/edit/:courseId", async (req, res) => {
-    const course = await courseService.getCurrentCourse(req.params.courseId).lean();
-    res.render("edit", { layout: false, course })
+    try {
+        const course = await courseService.getCurrentCourse(req.params.courseId).lean();
+        res.render("edit", { layout: false, course })
+    } catch (error) {
+        let errorMess = getErrorMessage(error)
+        res.render(`404`, { layout: false, error: errorMess })
+    }
 })
 
 router.post("/edit/:courseId", async (req, res) => {
@@ -50,14 +69,19 @@ router.post("/edit/:courseId", async (req, res) => {
         await courseService.updateCourse(req.params.courseId, req.body)
         res.redirect(`/details/${req.params.courseId}`)
     } catch (error) {
-        res.render(`edit`, { layout: false, course: req.body, error: error.message })
+        let errorMess = getErrorMessage(error)
+        res.render(`edit`, { layout: false, course: req.body, error: errorMess })
     }
-
 })
 
 router.get("/delete/:courseId", async (req, res) => {
-    await courseService.deleteCourse(req.params.courseId);
-    res.redirect("/courses")
+    try {
+        await courseService.deleteCourse(req.params.courseId);
+        res.redirect("/courses")
+    } catch (error) {
+        let errorMess = getErrorMessage(error)
+        res.render(`404`, { layout: false, error: errorMess })
+    }
 })
 
 module.exports = router
